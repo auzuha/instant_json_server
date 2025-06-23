fe721f31ea90fe40439d2da0ec6a7ef361c48e91
Augustine Stephens <42874008+auzuha@users.noreply.github.com>
2025-06-23 13:27:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,44 @@ setup(
         ],
     },
     author_email='auzuha@gmail.com',
-    description='Package to create backend API server with all basic CRUD operations for any JSON File.',
+    description='''Package to create backend API server with all basic CRUD operations for any JSON File.
+    Meant for developers who need to quickly wind up a server for a json file. It creates all basic CRUD endpoints for each resource of a JSON file of your choice.
+
+    To Install
+    pip install instant-json-server
+    To use
+    instant-json-server path_to_your_json
+    Example JSON File that works:
+      {
+        "blogs": [
+            {
+                "id": 1,
+                "content": "first blog"
+            },
+            {
+                "id": 2,
+                "content": "second blog"
+            }
+        ],
+        "users": [
+            {
+                "id": "usr1",
+                "username": "username1"
+            },
+            {
+                "id": "usr2",
+                "username": "username2"
+            },
+            {
+                "id": 1233,
+                "username": "asdasdad"
+            },
+            {
+                "id": 12234234242433,
+                "username": "asdasdad"
+            }
+        ]
+    }''',
     long_description='Package to create backend API server with all basic CRUD operations.',
     long_description_content_type='text/markdown',
     url='https://github.com/auzuha/instant_json_server',
