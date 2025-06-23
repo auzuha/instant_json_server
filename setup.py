6103d277f52a09482280abe8098ab4fa67b6d2da
Augustine Stephens <42874008+auzuha@users.noreply.github.com>
2025-06-23 13:28:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
         ],
     },
     author_email='auzuha@gmail.com',
-    description='''Package to create backend API server with all basic CRUD operations for any JSON File.
+    description='Package to create backend API server with all basic CRUD operations for any JSON File.',
+    long_description='''Package to create backend API server with all basic CRUD operations for any JSON File.
     Meant for developers who need to quickly wind up a server for a json file. It creates all basic CRUD endpoints for each resource of a JSON file of your choice.
 
     To Install
@@ -53,7 +54,6 @@ setup(
             }
         ]
     }''',
-    long_description='Package to create backend API server with all basic CRUD operations.',
     long_description_content_type='text/markdown',
     url='https://github.com/auzuha/instant_json_server',
     classifiers=[
